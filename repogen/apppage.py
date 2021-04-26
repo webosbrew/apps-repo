465cefd8c93f6894b6f6521802d311ad29a82da6
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-26 11:26:59 +0900

    Code cleanup

--- repogen/apppage.py
+++ repogen/apppage.py
@@ -1,33 +1,36 @@
-import argparse
 import json
 from os import makedirs
 from os.path import exists, join
 
 from repogen.common import list_packages
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input-dir', required=True)
-parser.add_argument('-o', '--output-dir', required=True)
-args = parser.parse_args()
 
-pkgpath = args.input_dir
-outdir = args.output_dir
+def generate(indir, outdir):
+    packages = list_packages(indir)
 
-packages = list_packages(pkgpath)
+    if not exists(outdir):
+        makedirs(outdir)
 
-if not exists(outdir):
-    makedirs(outdir)
+    template = '''\
+    Title: {title}
+    status: hidden
+    save_as: apps/{id}.html
 
-template = '''\
-Title: {title}
-status: hidden
-save_as: apps/{id}.html
+    This is description of an application
+    '''
+    for pkg in packages:
+        with open(join(outdir, '%s.md' % pkg['id']), 'w') as f:
+            f.write(template.format_map(pkg))
 
-This is description of an application
-'''
+    print('Generated application page for %d packages.' % len(packages))
 
-for pkg in packages:
-    with open(join(outdir, '%s.md' % pkg['id']), 'w') as f:
-        f.write(template.format_map(pkg))
 
-print('Generated for %d packages.' % len(packages))
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-dir', required=True)
+    parser.add_argument('-o', '--output-dir', required=True)
+    args = parser.parse_args()
+
+    generate(args.input_dir, args.output_dir)
--- repogen/jsondata.py
+++ repogen/jsondata.py
@@ -1,26 +1,30 @@
-import argparse
 import json
 from os import makedirs
 from os.path import exists, join
 
 from repogen.common import list_packages
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input-dir', required=True)
-parser.add_argument('-o', '--output-dir', required=True)
-args = parser.parse_args()
 
-pkgpath = args.input_dir
-outdir = args.output_dir
+def generate(indir, outdir):
+    packages = list_packages(indir)
 
-packages = list_packages(pkgpath)
+    if not exists(outdir):
+        makedirs(outdir)
 
-if not exists(outdir):
-    makedirs(outdir)
+    with open(join(outdir, 'apps.json'), 'w') as f:
+        json.dump({
+            'packages': packages
+        }, f, indent=2)
 
-with open(join(outdir, 'apps.json'), 'w') as f:
-    json.dump({
-        'packages': packages
-    }, f, indent=2)
+    print('Generated json data for %d packages.' % len(packages))
 
-print('Generated for %d packages.' % len(packages))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-dir', required=True)
+    parser.add_argument('-o', '--output-dir', required=True)
+    args = parser.parse_args()
+
+    generate(args.input_dir, args.output_dir)
