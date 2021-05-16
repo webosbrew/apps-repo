3d9cd97b3d6d06f4ab2285f5b974c43f7e5d01e9
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-05-16 16:24:33 +0900

    Manifest caching WIP

--- repogen/common.py
+++ repogen/common.py
@@ -1,10 +1,26 @@
-from os import listdir
+import json
+from os import listdir, path
 from os.path import basename, isfile, join
 from urllib.parse import urljoin
 
 import bleach
 import requests
 import yaml
+
+
+def obtain_manifest(pkgid, url: str):
+    cache_file = path.join('cache', 'manifest_%s.json' % pkgid)
+    try:
+        manifest = requests.get(url=url, allow_redirects=True).json()
+        manifest['ipkUrl'] = urljoin(url, manifest['ipkUrl'])
+        with open(cache_file, 'w') as f:
+            json.dump(manifest, f)
+        return manifest
+    except requests.exceptions.RequestException:
+        if (path.exists(cache_file)):
+            with open(cache_file) as f:
+                return json.load(f)
+    return None
 
 
 def parse_package_info(path: str):
@@ -17,18 +33,18 @@ def parse_package_info(path: str):
     pkgid = filename[:suffixidx]
     if not ('title' in content) and ('iconUri' in content) and ('manifestUrl' in content):
         return None
-    manifest = requests.get(
-        url=content['manifestUrl'], allow_redirects=True).json()
-    manifest['ipkUrl'] = urljoin(content['manifestUrl'], manifest['ipkUrl'])
-    return {
+    pkginfo = {
         'id': pkgid,
         'title': content['title'],
         'iconUri': content['iconUri'],
         'manifestUrl': content['manifestUrl'],
-        'manifest': manifest,
         'category': content['category'],
         'description': bleach.clean(content.get('description', '')),
     }
+    manifest = obtain_manifest(pkgid, content['manifestUrl'])
+    if manifest:
+        pkginfo['manifest'] = manifest
+    return pkginfo
 
 
 def list_packages(pkgdir):
