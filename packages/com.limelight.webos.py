13d0613ba20fa31f46a3a14f3d937e264510324a
Mariotaku <mariotaku.lee@gmail.com>
2021-12-28 00:46:09 +0900

    Update com.limelight.webos.py

--- packages/com.limelight.webos.py
+++ packages/com.limelight.webos.py
@@ -5,20 +5,21 @@ iconUri = 'https://github.com/mariotaku/moonlight-tv/raw/main/deploy/webos/icon.
 manifestUrl = 'https://github.com/mariotaku/moonlight-tv/releases/latest/download/com.limelight.webos.manifest.json'
 category = 'games'
 description = '''
-Moonlight TV is a GUI front end for [Moonlight GameStream Client](https://moonlight-stream.org/). With some components from [moonlight-embedded](https://github.com/irtimmer/moonlight-embedded).
-It was originally designed for LG webOS TVs, but may support running on more devices in the future.
+Moonlight TV is a community version of [Moonlight GameStream Client](https://moonlight-stream.org/), made for large
+screens.
 
 ## Features
 
+* High performance streaming for webOS
+* UI optimized for large screen and remote controller
 * Supports up to 4 controllers
-* Utilizes system hardware decoder to get best performance (webOS 3/4/5)
-* Easy to port to other OSes (Now runs on macOS, Linux, Raspberry Pi)
+* Easy to port to other OSes (Now runs on macOS, Arch, Debian, Raspbian and Windows)
 
 ## Screenshots
 
 ![Launcher](https://user-images.githubusercontent.com/830358/141690137-529d3b94-b56a-4f24-a3c5-00a56eb30952.png)
 
-![Settings](https://user-images.githubusercontent.com/830358/141690143-6752757b-5e1f-4a23-acf7-4b2667e2fd05.png)
+![Settings](https://user-images.githubusercontent.com/830358/147389849-6907f614-dbd4-4c24-987e-1a214a9680d0.png)
 
 ![In-game Overlay](https://user-images.githubusercontent.com/830358/141690146-27ee2564-0cc8-43ef-a5b0-54b8487dda1e.png)
 _Screenshot performed on TV has lower picture quality. Actual picture quality is better._
