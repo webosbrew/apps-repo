efde2a8079c4899a69f6e00b4a9105aa6b3ee5f2
Mariotaku <mariotaku.lee@gmail.com>
2023-10-02 23:04:32 +0900

    code cleanup

--- repogen/siteurl.py
+++ repogen/siteurl.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def siteurl() -> str:
-    cname_file = Path(__file__, '..', '..', 'content', 'extra', 'CNAME')
+    cname_file = Path(__file__).parent.parent.joinpath('content', 'extra', 'CNAME')
     if not cname_file.exists():
         return ''
     return f'https://{cname_file.read_text().strip()}'
