13e2e53705a68f62f47a7925ce30307d22d09518
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2023-12-12 00:28:48 -0500

    hack site URL into siteurl.py

--- repogen/siteurl.py
+++ repogen/siteurl.py
@@ -4,5 +4,6 @@ from pathlib import Path
 def siteurl() -> str:
     cname_file = Path(__file__).parent.parent.joinpath('content', 'extra', 'CNAME')
     if not cname_file.exists():
-        return ''
+        # XXX: hack
+        return 'https://throwaway96.github.io/apps-repo'
     return f'https://{cname_file.read_text().strip()}'
