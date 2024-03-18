e7d91520241c89e8f17b221ad9ceb3819d5b7f25
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-03-17 20:54:38 -0400

    CI: make package lint more robust
    Includes some fixes in obtain_manifest().

--- repogen/downloadipk.py
+++ repogen/downloadipk.py
@@ -1,8 +1,12 @@
+from sys import stderr, exit
 from pathlib import Path
+import json.decoder
 
 import requests
+import requests.exceptions
 
 from repogen import pkg_info
+from jsonschema.exceptions import ValidationError
 
 if __name__ == '__main__':
     import argparse
@@ -12,8 +16,42 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', required=True)
     args = parser.parse_args()
 
-    pkginfo = pkg_info.from_package_info_file(Path(args.info))
-    with requests.get(pkginfo['manifest']['ipkUrl'], allow_redirects=True) as resp:
-        with open(args.output, 'wb') as f:
-            f.write(resp.content)
-            print(f'IPK file downloaded: {args.output}')
+    try:
+        pkginfo = pkg_info.from_package_info_file(Path(args.info))
+    except (requests.exceptions.JSONDecodeError, json.decoder.JSONDecodeError) as e:
+        print(f'Could not parse manifest: {e}')
+        exit(2)
+    except (IOError) as e:
+        print(f'Could not open package info file: {e.strerror}')
+        exit(3)
+    except ValidationError as e:
+        print(f'Could not parse package info: {e.message}')
+        exit(4)
+
+    if pkginfo is None:
+        print(f'Failed to get manifest for unknown reason')
+        exit(5)
+
+    try:
+        ipk_url = pkginfo['manifest']['ipkUrl']
+    except KeyError as e:
+        print(f'Invalid package info: missing key {e}')
+        exit(6)
+
+    try:
+        with requests.get(ipk_url, allow_redirects=True) as resp:
+            try:
+                with open(args.output, 'wb') as f:
+                    try:
+                        f.write(resp.content)
+                    except IOError as e:
+                        print(f'Could not write to output IPK file: {e.strerror}')
+                        exit(9)
+                    else:
+                        print(f'IPK file downloaded: {args.output}', file=stderr)
+            except IOError as e:
+                print(f'Could not open output IPK file: {e.strerror}')
+                exit(8)
+    except requests.exceptions.RequestException as e:
+        print(f'Could not download IPK: {e}')
+        exit(7)
